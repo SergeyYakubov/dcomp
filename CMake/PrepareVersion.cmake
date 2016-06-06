fed8d4e68785e917db83fcfe63456805f658d18e
Sergey Yakubov <sergey.yakubov@desy.de>
2016-06-06 17:48:18 +0200

    Build via CMake, versioning

---
+++ CMake/PrepareVersion.cmake
@@ -0,0 +1,16 @@
+# set path for modules
+
+
+execute_process(COMMAND git describe --tags --dirty OUTPUT_VARIABLE VERSION)
+string(STRIP ${VERSION} VERSION)
+
+string(REGEX REPLACE "^v([0-9]+)\\..*" "\\1" VERSION_MAJOR "${VERSION}")
+string(REGEX REPLACE "^v[0-9]+\\.([0-9]+).*" "\\1" VERSION_MINOR "${VERSION}")
+string(REGEX REPLACE "^v[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" VERSION_PATCH "${VERSION}")
+string(REGEX REPLACE "^v[0-9]+\\.[0-9]+\\.[0-9]+(.*)" "\\1" VERSION_SHA1 "${VERSION}")
+set(VERSION_SHORT "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}")
+
+string(TIMESTAMP TIMESTAMP "%H:%M:%S %d.%m.%Y UTC" UTC)
+
+configure_file(source/version/version_lib.go.in source/version/version_lib.go)
+
--- source/cmd/dcomp.go
+++ source/cmd/dcomp.go
@@ -1,11 +1,35 @@
 package main
 
-import "fmt"
+import (
+	dcompversion "../version"
+	"fmt"
+	flag "github.com/docker/docker/pkg/mflag"
+)
+
+var (
+	flHelp    = flag.Bool([]string{"h", "-help"}, false, "Print usage")
+	flVersion = flag.Bool([]string{"v", "-version"}, false, "Print version information and quit")
+)
 
 func main() {
-	fmt.Println("Hello\n")
+
+	flag.Parse()
+
+	if *flVersion {
+		showVersion()
+		return
+	}
+
+	if *flHelp {
+		flag.Usage()
+		return
+	}
 }
 
 func Dummy() int {
 	return 2
+}
+
+func showVersion() {
+	fmt.Printf("dComp version %s, build at %s\n", dcompversion.Version, dcompversion.BuildTime)
 }
