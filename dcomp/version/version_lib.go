f8ad1c51f1112fcaa8806ec55d98223994bf8e10
Sergey Yakubov <sergey.yakubov@desy.de>
2016-07-04 14:04:14 +0200

    add version_lib

---
+++ dcomp/version/version_lib.go
@@ -0,0 +1,10 @@
+package version
+
+// Default build-time variable for library-import.
+// This file is overridden on build with build-time informations.
+const (
+	GitCommit string = "-15-g4f7664c-dirty"
+	Version   string = "v0.0.1-15-g4f7664c-dirty"
+	ShortVersion   string = "0.0.1"
+	BuildTime string = "11:53:06 04.07.2016 UTC"
+)
